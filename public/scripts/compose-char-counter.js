3df99d4bb5f845ef7491c2d296c186cc190792f0
Dennis Law <denn15_law@live.ca>
2021-11-16 21:11:14 +0000

    add function logic to adjust the char count and change css to red when over char limit

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#tweet-text").keyup(function (e) {
+    const counter = $(this).next().children(".counter");
+    counter.text(140 - $(this).val().length);
+    const val = $(this).val().length;
+    if (val > 140) {
+      counter.css("color", "red");
+    }
+  });
+});
