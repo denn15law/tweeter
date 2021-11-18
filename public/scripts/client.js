cc65116e99583f7ad57fa7626c3f2cc28ff182d1
Dennis Law <denn15_law@live.ca>
2021-11-18 23:20:58 +0000

    add scroll to top button functionailty

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -99,9 +99,22 @@ $(document).ready(function () {
     $("#tweet-text").focus();
   });
 
-  $(".rotate").click(function () {
-    $(this).toggleClass("down");
+  //Stretch: Back to Top Button Functionality
+  $(".top-btn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
   });
+
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      $(".top-btn").css("display", "block");
+    } else {
+      $(".top-btn").css("display", "none");
+    }
+  };
 
   //hide alert initially
   $(".alert").hide();
